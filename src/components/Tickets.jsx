bd4ca71b5dbaff41a3846365ddfce5aec6f20f7c
Tennie <kayodeteniola1000@gmail.com>
2026-03-21 06:03:59 +0100

    regular ticket price done back

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -15,7 +15,7 @@ const tickets = [
     label: 'Open Access',
     name: 'General Access Ticket',
     subtitle: 'Experience the future of flexible work',
-    price: 100,
+    price: 3000,
     available: 100,  // ADD THIS
     features: [
       'Access to main event space',
