567c18b2793a0af828088f4b11f874a70de44d43
Tennie <kayodeteniola100@gmail.com>
2026-07-13 10:02:37 +0100

    updated stella photo path

--- src/components/speakers.jsx
+++ src/components/speakers.jsx
@@ -168,7 +168,7 @@ const speakers = [
 
   {
     id: 8,
-    name: 'Stella Ijeoma Olugbemi',
+    name: 'Stella Ijeoma Olugbemi2',
     connexerType: 'BUSINESS CONNEXER',
     title: 'CSR, Brand and Events Manager',
     company: 'Egbin Power Plc',
